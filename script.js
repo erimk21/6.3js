bc42561f85dfb33435af9858eac8f42bb40f2d87
erimk21 <9019259@student.zadkine.nl>
2022-10-20 09:55:05 +0200

    opzet 6.3

---
+++ script.js
@@ -0,0 +1,5 @@
+function KleurAanpassen(){
+    console.log("Het werkt!");
+    document.getElementById("kleur").style.color = "red";
+    document.getElementById("kleur").innerHTML = "tekst zal van kleur veranderen.";
+}
